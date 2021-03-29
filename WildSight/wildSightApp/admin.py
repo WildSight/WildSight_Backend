59ab45de630fa911a9f963c28347e412bb35ecd9
anmol <anmolpreet.bhatia@gmail.com>
2021-03-29 12:08:59 +0530

    Add Species and location model. Fill species database

--- WildSight/wildSightApp/admin.py
+++ WildSight/wildSightApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Location, Species
+
+admin.site.register(Location)
+admin.site.register(Species)
--- WildSight/wildSightApp/models.py
+++ WildSight/wildSightApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    x_coordinate_start=models.DecimalField(max_digits=9, decimal_places=6)
+    x_coordinate_end=models.DecimalField(max_digits=9, decimal_places=6)
+    y_coordinate_start=models.DecimalField(max_digits=9, decimal_places=6)
+    y_coordinate_end=models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return "Location centred at: {}, {}".format((self.x_coordinate_start+self.x_coordinate_end)/2, (self.y_coordinate_start+self.y_coordinate_end)/2 )
+
+
+class Species(models.Model):
+    
+    class Meta:
+        verbose_name_plural="Species"
+
+    common_name=models.CharField(max_length=50)
+    scientific_name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.common_name
+
